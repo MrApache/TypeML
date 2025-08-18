ac2f3374bbd0d532f1899734db0a8ce5e9754cea
Irisu <mrapacheee@gmail.com>
2025-08-18 16:01:09 +0400

    fix path to lsp

--- make-debug.sh
+++ make-debug.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 cargo build
-cargo install --path .
+cargo install --path ./crates/rml-lsp
--- make-release.sh
+++ make-release.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 cargo build --release
-cargo install --path .
+cargo install --path ./crates/rml-lsp/
