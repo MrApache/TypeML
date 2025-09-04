d7c1d30a28dbebb547d5ed7a36d0c74ac1e85735
Irisu <mrapacheee@gmail.com>
2025-09-04 16:29:32 +0400

    include file content instead of i/o

--- crates/rmlx/benches/benchmark.rs
+++ crates/rmlx/benches/benchmark.rs
@@ -1,13 +1,11 @@
 use rmlx::SchemaAst;
 
 fn main() {
-    // Run registered benchmarks.
     divan::main();
 }
 
 #[divan::bench]
 fn parse_ast() {
-    let path = concat!(env!("CARGO_WORKSPACE_DIR"), "examples/schema.rmlx");
-    let content = std::fs::read_to_string(path).expect("Failed to read file");
-    let _ast = SchemaAst::new(&content);
+    const CONTENT: &str = include_str!(concat!(env!("CARGO_WORKSPACE_DIR"), "examples/schema.rmlx"));
+    let _ast = SchemaAst::new(CONTENT);
 }
