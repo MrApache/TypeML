f71094a4feb084a19191f6e0e1d6d5b9c48f6eaf
Irisu <mrapacheee@gmail.com>
2025-10-14 17:10:46 +0400

    Create a tests crate

---
+++ crates/rml_tests/tests/full.rs
@@ -0,0 +1,13 @@
+#[cfg(test)]
+mod tests {
+    use rml::{LayoutModel, RmlParser};
+    const PATH: &str = concat!(env!("CARGO_WORKSPACE_DIR"), "examples/layout.rml");
+    #[test]
+    fn test() {
+        let content = std::fs::read_to_string(PATH).unwrap();
+        let ast = RmlParser::build_ast(&content);
+        assert!(ast.is_ok(), "{}", ast.unwrap_err());
+        let result = LayoutModel::validate(ast.unwrap(), PATH);
+        assert!(result.is_ok(), "{}", result.unwrap_err());
+    }
+}
